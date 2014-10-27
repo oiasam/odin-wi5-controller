dae609ab4459aa71d5c99c50b782042b48a5fe27
Fran Gonzalez <francisco.gonzalez.garcia@bisdn.de>
2014-10-27 16:33:10 +0100

    DHCP is not filtered, switch 1->2, 2->1

--- src/main/java/net/floodlightcontroller/odin/master/OdinAgent.java
+++ src/main/java/net/floodlightcontroller/odin/master/OdinAgent.java
@@ -54,43 +54,43 @@ class OdinAgent implements IOdinAgent {
 	private final int RX_STAT_NUM_PROPERTIES = 5;
 	private final int ODIN_AGENT_PORT = 6777;
 
-	
+
 	/**
 	 * Probably need a better identifier
-	 * 
+	 *
 	 * @return the agent's IP address
 	 */
 	public InetAddress getIpAddress() {
 		return ipAddress;
 	}
 
-	
+
 	/**
 	 * Returns timestamp of last heartbeat from agent
-	 * 
+	 *
 	 * @return Timestamp
 	 */
 	public long getLastHeard() {
 		return lastHeard;
 	}
 
-	
+
 	/**
 	 * Set the lastHeard timestamp of a client
-	 * 
+	 *
 	 * @param t  timestamp to update lastHeard value
 	 */
 	public void setLastHeard(long t) {
 		this.lastHeard = t;
 	}
 
-	
+
 	/**
 	 * Probe the agent for a list of VAPs its hosting. This should only be used
 	 * by the master when an agent registration to shield against master
 	 * failures. The assumption is that when this is invoked, the controller has
 	 * never heard about the agent before.
-	 * 
+	 *
 	 * @return a list of OdinClient entities on the agent
 	 */
 	public Set<OdinClient> getLvapsRemote() {
@@ -107,8 +107,8 @@ class OdinAgent implements IOdinAgent {
 
 			if (entry.equals(""))
 				break;
-			
-			/* 
+
+			/*
 			 * Every entry looks like this:
 			 * properties:  [0]       [1]         [2]         [3, 4, 5...]
 			 *           <sta_mac> <ipv4addr> <lvap bssid> <lvap ssid list>
@@ -128,7 +128,7 @@ class OdinAgent implements IOdinAgent {
 						InetAddress.getByName(properties[1]), lvap);
 				lvap.setAgent(this);
 				clients.add(oc);
-				
+
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
 			}
@@ -139,28 +139,28 @@ class OdinAgent implements IOdinAgent {
 		return clients;
 	}
 
-	
+
 	/**
 	 * Return a list of LVAPs that the master knows this agent is hosting.
 	 * Between the time an agent has crashed and the master detecting the crash,
 	 * this can return stale values.
-	 * 
+	 *
 	 * @return a list of OdinClient entities on the agent
 	 */
 	public Set<OdinClient> getLvapsLocal() {
 		return clientList;
 	}
 
-	
+
 	/**
 	 * Retrive Rx-stats from the OdinAgent.
-	 * 
+	 *
 	 * @return A map of stations' MAC addresses to a map of properties and
 	 *         values.
 	 */
 	public Map<MACAddress, Map<String, String>> getRxStats() {
 		String stats = invokeReadHandler(READ_HANDLER_RXSTATS);
-		
+
 		Map<MACAddress, Map<String, String>> ret = new HashMap<MACAddress, Map<String, String>>();
 
 		/*
@@ -189,46 +189,88 @@ class OdinAgent implements IOdinAgent {
 		return Collections.unmodifiableMap(ret);
 	}
 
-	
+
 	/**
 	 * To be called only once, initialises a connection to the OdinAgent's
 	 * control socket. We let the connection persist so as to save on
 	 * setup/tear-down messages with every invocation of an agent. This will
 	 * also help speedup handoffs.
-	 * 
+	 *
 	 * @param host Click based OdinAgent host
 	 * @param port Click based OdinAgent's control socket port
 	 * @return 0 on success, -1 otherwise
 	 */
 	public int init(InetAddress host) {
+		/*
 		OFFlowMod flow2 = new OFFlowMod();
 		{
 			OFMatch match = new OFMatch();
 			match.fromString("dl_type=0x0800,nw_proto=17,tp_dst=68");
-			
+
 			OFActionOutput actionOutput = new OFActionOutput ();
 			actionOutput.setPort(OFPort.OFPP_CONTROLLER.getValue());
 			actionOutput.setLength((short) OFActionOutput.MINIMUM_LENGTH);
-			
+
 			List<OFAction> actionList = new ArrayList<OFAction>();
 			actionList.add(actionOutput);
-			
-		
+
+
 			flow2.setCookie(67);
 			flow2.setPriority((short) 200);
 			flow2.setMatch(match);
 			flow2.setIdleTimeout((short) 0);
 			flow2.setActions(actionList);
 	        flow2.setLength(U16.t(OFFlowMod.MINIMUM_LENGTH + OFActionOutput.MINIMUM_LENGTH));
+		}*/
+
+		OFFlowMod flow1 = new OFFlowMod();
+		{
+			OFMatch match = new OFMatch();
+			match.fromString("in_port=1,dl_type=0x0800");
+
+			OFActionOutput actionOutput = new OFActionOutput ();
+			actionOutput.setPort((short) 2);
+			actionOutput.setLength((short) OFActionOutput.MINIMUM_LENGTH);
+
+			List<OFAction> actionList = new ArrayList<OFAction>();
+			actionList.add(actionOutput);
+
+			flow1.setCookie(67);
+			flow1.setPriority((short) 200);
+			flow1.setMatch(match);
+			flow1.setIdleTimeout((short) 0);
+			flow1.setActions(actionList);
+			flow1.setLength(U16.t(OFFlowMod.MINIMUM_LENGTH + OFActionOutput.MINIMUM_LENGTH));
 		}
-		
+
+		OFFlowMod flow2 = new OFFlowMod();
+		{
+			OFMatch match = new OFMatch();
+			match.fromString("in_port=2,dl_type=0x0800");
+
+			OFActionOutput actionOutput = new OFActionOutput ();
+			actionOutput.setPort((short) 1);
+			actionOutput.setLength((short) OFActionOutput.MINIMUM_LENGTH);
+
+			List<OFAction> actionList = new ArrayList<OFAction>();
+			actionList.add(actionOutput);
+
+			flow2.setCookie(67);
+			flow2.setPriority((short) 200);
+			flow2.setMatch(match);
+			flow2.setIdleTimeout((short) 0);
+			flow2.setActions(actionList);
+			flow2.setLength(U16.t(OFFlowMod.MINIMUM_LENGTH + OFActionOutput.MINIMUM_LENGTH));
+		}
+
 		try {
+			ofSwitch.write(flow1, null);
 			ofSwitch.write(flow2, null);
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+
 		try {
 			odinAgentSocket = new Socket(host.getHostAddress(), ODIN_AGENT_PORT);
 			outBuf = new PrintWriter(odinAgentSocket.getOutputStream(), true);
@@ -246,30 +288,30 @@ class OdinAgent implements IOdinAgent {
 		return 0;
 	}
 
-	
+
 	/**
 	 * Get the IOFSwitch for this agent
-	 * 
+	 *
 	 * @return ofSwitch
 	 */
 	public IOFSwitch getSwitch() {
 		return ofSwitch;
 	}
 
-	
+
 	/**
 	 * Set the IOFSwitch entity corresponding to this agent
-	 * 
+	 *
 	 * @param sw the IOFSwitch entity for this agent
 	 */
 	public void setSwitch(IOFSwitch sw) {
 		ofSwitch = sw;
 	}
 
-	
+
 	/**
 	 * Remove a virtual access point from the AP corresponding to this agent
-	 * 
+	 *
 	 * @param staHwAddr The STA's ethernet address
 	 */
 	public void removeClientLvap(OdinClient oc) {
@@ -278,51 +320,51 @@ class OdinAgent implements IOdinAgent {
 		clientList.remove(oc);
 	}
 
-	
+
 	/**
 	 * Add a virtual access point to the AP corresponding to this agent
-	 * 
+	 *
 	 * @param oc OdinClient entity
 	 */
 	public void addClientLvap(OdinClient oc) {
 		assert (oc.getLvap() != null);
-		
+
 		String ssidList = "";
-		
+
 		for (String ssid: oc.getLvap().getSsids()) {
 			ssidList += " " + ssid;
 		}
-		
+
 		invokeWriteHandler(WRITE_HANDLER_ADD_VAP, oc.getMacAddress().toString()
 				+ " " + oc.getIpAddress().getHostAddress() + " "
 				+ oc.getLvap().getBssid().toString() + ssidList);
 		clientList.add(oc);
 	}
 
-	
+
 	/**
 	 * Update a virtual access point with possibly new IP, BSSID, or SSID
-	 * 
+	 *
 	 * @param oc OdinClient entity
 	 */
 	public void updateClientLvap(OdinClient oc) {
 		assert (oc.getLvap() != null);
-		
+
 		String ssidList = "";
-		
+
 		for (String ssid: oc.getLvap().getSsids()) {
 			ssidList += " " + ssid;
 		}
-		
+
 		invokeWriteHandler(WRITE_HANDLER_SET_VAP, oc.getMacAddress().toString()
 				+ " " + oc.getIpAddress().getHostAddress() + " "
 				+ oc.getLvap().getBssid().toString() + ssidList);
 	}
 
-	
+
 	/**
 	 * Set subscriptions
-	 * 
+	 *
 	 * @param subscriptions
 	 * @param t timestamp to update lastHeard value
 	 */
@@ -330,10 +372,10 @@ class OdinAgent implements IOdinAgent {
 		invokeWriteHandler(WRITE_HANDLER_SUBSCRIPTIONS, subscriptionList);
 	}
 
-	
+
 	/**
 	 * Internal method to invoke a read handler on the OdinAgent
-	 * 
+	 *
 	 * @param handlerName OdinAgent handler
 	 * @return read-handler string
 	 */
@@ -367,10 +409,10 @@ class OdinAgent implements IOdinAgent {
 		return null;
 	}
 
-	
+
 	/**
 	 * Internal method to invoke a write handler of the OdinAgent
-	 * 
+	 *
 	 * @param handlerName OdinAgent write handler name
 	 * @param handlerText Write string
 	 */
@@ -387,7 +429,7 @@ class OdinAgent implements IOdinAgent {
 		sb.append(clientHwAddr);
 		sb.append(" ");
 		sb.append(bssid);
-		
+
 		for (String ssid: ssidList) {
 			sb.append(" ");
 			sb.append(ssid);
