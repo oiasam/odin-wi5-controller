37ba62b9cb7d5c9e48c6ff225dcca86d9b876450
Fran Gonzalez <francisco.gonzalez.garcia@bisdn.de>
2014-10-31 13:48:29 +0100

    dhcp mgmt

--- src/main/java/net/floodlightcontroller/odin/master/OdinAgent.java
+++ src/main/java/net/floodlightcontroller/odin/master/OdinAgent.java
@@ -229,9 +229,9 @@ class OdinAgent implements IOdinAgent {
 		OFFlowMod flow1 = new OFFlowMod();
 		{
 			OFMatch match = new OFMatch();
-			match.fromString("in_port=1,dl_type=0x0800");
+			//match.fromString("in_port=1,dl_type=0x0800");
 			//match.fromString("in_port=1, dl_type=0x0800,nw_proto=17,tp_dst=68"); //port 68 used by DHCP client, 67 by server
-			//match.fromString("in_port=1");
+			match.fromString("in_port=1");
 
 			OFActionOutput actionOutput = new OFActionOutput ();
 			actionOutput.setPort((short) 2);
@@ -251,9 +251,9 @@ class OdinAgent implements IOdinAgent {
 		OFFlowMod flow2 = new OFFlowMod();
 		{
 			OFMatch match = new OFMatch();
-			match.fromString("in_port=2,dl_type=0x0800");
+			//match.fromString("in_port=2,dl_type=0x0800");
 			//match.fromString("in_port=2, dl_type=0x0800,nw_proto=17,tp_dst=67"); //port 68 used by DHCP client, 67 by server
-			//match.fromString("in_port=2");
+			match.fromString("in_port=2");
 
 			OFActionOutput actionOutput = new OFActionOutput ();
 			actionOutput.setPort((short) 1);
