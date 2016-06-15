6805fdaf921a84fc41ac3279cd87694fc81a0f6a
Jose Saldana <jsaldana@unizar.es>
2016-06-15 17:45:39 +0200

    Selecting only associated STAs

--- src/main/java/net/floodlightcontroller/odin/applications/ShowStatistics.java
+++ src/main/java/net/floodlightcontroller/odin/applications/ShowStatistics.java
@@ -36,22 +36,55 @@ public class ShowStatistics extends OdinApplication {
 				 
 	  // for each Agent
 	  for (InetAddress agentAddr: getAgents()) {
-	    Map<MACAddress, Map<String, String>> vals = getRxStatsFromAgent(agentAddr);
 	    System.out.println("\nAgent: " + agentAddr);
+	    
+	    // Reception statistics
+	    Map<MACAddress, Map<String, String>> vals = getRxStatsFromAgent(agentAddr);
+
 
             // for each STA associated to the Agent
-	    for (Entry<MACAddress, Map<String, String>> vals_entry: vals.entrySet()) {
-	      MACAddress staHwAddr = vals_entry.getKey();
-	      System.out.println("\tStation: " + staHwAddr );
-	      System.out.println("\t\tnum packets: " + vals_entry.getValue().get("packets"));
-	      System.out.println("\t\tavg rate: " + vals_entry.getValue().get("avg_rate") + "kbps");
-	      System.out.println("\t\tavg signal: " + vals_entry.getValue().get("avg_signal") + "dBm");
-	      System.out.println("\t\tavg length: " + vals_entry.getValue().get("avg_len_pkt") + "bytes");
-	      System.out.println("\t\tair time: " + vals_entry.getValue().get("air_time") + "ms");						
-	      System.out.println("\t\tinit time: " + vals_entry.getValue().get("first_received") + "sec");
-	      System.out.println("\t\tend time: " + vals_entry.getValue().get("last_received") + "sec");
-	      System.out.println("");
+	    for (Entry<MACAddress, Map<String, String>> vals_entry_rx: vals.entrySet()) {
+	      MACAddress staHwAddr = vals_entry_rx.getKey();
+	      
+	      for (OdinClient oc_rx: clients) {
+		if (oc_rx.getMacAddress().equals(staHwAddr) && oc_rx.getIpAddress() != null && !oc_rx.getIpAddress().getHostAddress().equals("0.0.0.0")) {
+	          System.out.println("\tStation: " + staHwAddr );
+	          System.out.println("\t\tnum packets: " + vals_entry_rx.getValue().get("packets"));
+	          System.out.println("\t\tavg rate: " + vals_entry_rx.getValue().get("avg_rate") + "kbps");
+	          System.out.println("\t\tavg signal: " + vals_entry_rx.getValue().get("avg_signal") + "dBm");
+	          System.out.println("\t\tavg length: " + vals_entry_rx.getValue().get("avg_len_pkt") + "bytes");
+	          System.out.println("\t\tair time: " + vals_entry_rx.getValue().get("air_time") + "ms");						
+	          System.out.println("\t\tinit time: " + vals_entry_rx.getValue().get("first_received") + "sec");
+	          System.out.println("\t\tend time: " + vals_entry_rx.getValue().get("last_received") + "sec");
+	          System.out.println("");
+		}
+	      }
 	    }
+	    
+	    // Transmission statistics
+	    Map<MACAddress, Map<String, String>> vals = getTxStatsFromAgent(agentAddr);
+
+
+            // for each STA associated to the Agent
+	    for (Entry<MACAddress, Map<String, String>> vals_entry_tx: vals.entrySet()) {
+	      MACAddress staHwAddr = vals_entry_tx.getKey();
+	      
+	      for (OdinClient oc_tx: clients) {
+		if (oc_tx.getMacAddress().equals(staHwAddr) && oc_tx.getIpAddress() != null && !oc_tx.getIpAddress().getHostAddress().equals("0.0.0.0")) {
+	          System.out.println("\tStation: " + staHwAddr );
+	          System.out.println("\t\tnum packets: " + vals_entry_tx.getValue().get("packets"));
+	          System.out.println("\t\tavg rate: " + vals_entry_tx.getValue().get("avg_rate") + "kbps");
+	          System.out.println("\t\tavg signal: " + vals_entry_tx.getValue().get("avg_signal") + "dBm");
+	          System.out.println("\t\tavg length: " + vals_entry_tx.getValue().get("avg_len_pkt") + "bytes");
+	          System.out.println("\t\tair time: " + vals_entry_tx.getValue().get("air_time") + "ms");						
+	          System.out.println("\t\tinit time: " + vals_entry_tx.getValue().get("first_received") + "sec");
+	          System.out.println("\t\tend time: " + vals_entry_tx.getValue().get("last_received") + "sec");
+	          System.out.println("");
+		}
+	      }
+	    }	    
+	    
+	    
 	  }
 	} catch (InterruptedException e) {
 	  e.printStackTrace();
