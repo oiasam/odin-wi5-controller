d2a2a213596c9d75a351de0417f116322bcecd09
Jose Saldana <jsaldana@unizar.es>
2016-08-29 13:42:28 +0200

    Improved ShowStatistics application
    Now it has an Initial interval and a reporting period.

--- src/main/java/net/floodlightcontroller/odin/applications/ShowStatistics.java
+++ src/main/java/net/floodlightcontroller/odin/applications/ShowStatistics.java
@@ -13,39 +13,60 @@ import net.floodlightcontroller.util.MACAddress;
 
 public class ShowStatistics extends OdinApplication {
 
+// IMPORTANT: this application only works if all the agents in the
+//poolfile are activated before the end of the INITIAL_INTERVAL.
+// Otherwise, the application looks for an object that does not exist
+//and gets stopped
+
 // this interval is for allowing the agents to connect to the controller
-private final int INTERVAL = 15000; // in ms
+private final int INITIAL_INTERVAL = 30000; // in ms
+
+// this interval is the period for showing the statistics
+private final int REPORTING_PERIOD = 15000; // in ms
 
 HashSet<OdinClient> clients;
 
   @Override
   public void run() {
+    try {
+			Thread.sleep(INITIAL_INTERVAL);
+		} catch (InterruptedException e) {
+	    e.printStackTrace();
+	  }
+
     while (true) {
       try {
-        Thread.sleep(INTERVAL);
-          clients = new HashSet<OdinClient>(getClients());
+        Thread.sleep(REPORTING_PERIOD);
+        clients = new HashSet<OdinClient>(getClients());
           /*
-	  * If a handoff has happened during the statistic gathering period,
-	  * it may happen that a client appears in the statistics of two agents
-	  * because it has been handed off from one agent to another
-	  */
+					* If a handoff has happened during the statistic gathering period,
+					* it may happen that a client appears in the statistics of two agents
+					* because it has been handed off from one agent to another
+					*/
+
+		System.out.println("[ShowStatistics] List of Agents:"); //JOSE
 
 	  // for each Agent
 	  for (InetAddress agentAddr: getAgents()) {
 	  
-	    System.out.println("\nAgent: " + agentAddr);
-	    
+	    System.out.println("[ShowStatistics] Agent: " + agentAddr);
+   
 		// Transmission statistics
+		System.out.println("[ShowStatistics] Last ping heard from agent " + agentAddr + " " + getLastHeardFromAgent(agentAddr));
+
 	    Map<MACAddress, Map<String, String>> vals_tx = getTxStatsFromAgent(agentAddr);  // all the clients who have statistics
-	    
+    
 		// Reception statistics
 	    Map<MACAddress, Map<String, String>> vals_rx = getRxStatsFromAgent(agentAddr);
 
 	    for (OdinClient oc: clients) {  // all the clients currently associated
 	    // NOTE: the clients currently associated MAY NOT be the same as the clients who have statistics
+
+	    System.out.println("\n[ShowStatistics] 1" + agentAddr);
 	    
          // for each STA associated to the Agent
 	     for (Entry<MACAddress, Map<String, String>> vals_entry_rx: vals_rx.entrySet()) {
+
 	      MACAddress staHwAddr = vals_entry_rx.getKey();
 			 if (oc.getMacAddress().equals(staHwAddr) && oc.getIpAddress() != null && !oc.getIpAddress().getHostAddress().equals("0.0.0.0")) {
 			     System.out.println("\tUplink station MAC: " + staHwAddr + " IP: " + oc.getIpAddress().getHostAddress());
@@ -73,13 +94,13 @@ HashSet<OdinClient> clients;
 	          System.out.println("\t\tend time: " + vals_entry_tx.getValue().get("last_received") + " sec");
 	          System.out.println("");
 	    	}
+
+	          }
+	        }	    
 	      }
-	    }	    
-	 
-	  }
-	} catch (InterruptedException e) {
-	  e.printStackTrace();
-	}
-      }
+	    } catch (InterruptedException e) {
+	      e.printStackTrace();
+	    }
     }
+  }
 }
